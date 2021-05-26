0894430f0b67eea43a79d88912063cf8a7e9322c
Rui Dias de Oliveira Filho <rui.dias@CTNDELL2CHF0D3.pampa.compasso>
2021-05-26 16:43:25 -0300

    atualizacao 20210526

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const router = express.Router();
@@ -23,7 +23,15 @@ router.post('/register', async(req, res) => {
 router.post('/authenticate', async (req, res)=>{
     const {email, password}  = req.body;
 
-    const user = await (await User.findOne({email})).isSelected('+password');
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user)
+        return res.status(400).send({error: 'User not found'});
+
+    if(!await bcrypt.compare(password, user.password));
+        return res.status(400).send({error: 'Invalid password'});
+
+    res.send({user});   
 });
 
 module.exports = app => app.use('/auth', router);
